89e8752d3d5c688612f359e55484f1542899b6b9
Mary Carmen Andrade <mary.andrade@tecsup.edu.pe>
2023-10-02 23:35:59 -0500

    Tarea Lab05 Andrade

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('Usuario conectado');
+
+    socket.on('chat message 1', function(msg){
+        console.log('Mensaje del chat 1: ' + msg);
+        io.emit('chat message 1', msg);
+    });
+
+    socket.on('chat message 2', function(msg){
+        console.log('Mensaje del chat 2: ' + msg);
+        io.emit('chat message 2', msg);
+    });
+
+    socket.on('disconnect', function(){
+        console.log('Usuario desconectado');
+    });
+});
+
+http.listen(3000, function(){
+    console.log('Servidor escuchando en http://localhost:3000')
+});
